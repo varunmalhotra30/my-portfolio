5c14e247e9378d5324a78e5c173c9819a4526d5f
Varun Malhotra <varun.malhotra@dcmail.ca>
2021-03-30 01:38:14 -0400

    complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
 // Navigation
+window.onload = function () {
+    let myPorfile = document.getElementById('obj').contentDocument;
+    let timeLineAnimation = gsap.timeline();
+    let dots = myPorfile.getElementsByClassName('randomCircle');
+    let letterM = myPorfile.getElementById('vmPath');
+    
+    let theColor = gsap.utils.random(['#ff6e40','#ffa476','#ffffa6','#cc41ff','peachpuff'],true);
+let colorList = ['#cf9a41','#f0b99a','#cc313d','#f7c5cc','#f599be','violet','gold'];
+
+    gsap.set(letterM, {
+        transformOrigin: "center center",
+            opacity:0,
+            x:20,
+            fill:"#000"
+        }); 
+        
+        gsap.to(letterM,{
+            x:0,
+            y:0,
+            fill:"#F2BB88",
+            opacity: 1,
+            duration: 2,
+            delay:4,
+            ease:"linear",
+    
+    })
+
+timeLineAnimation.set(dots,{
+fill: gsap.utils.wrap(colorList),
+
+attr:{
+    r: 'random(4,30)'
+
+},
+scale:0,
+x:'random(100, 800)',
+y: 'random(200,700)'
+
+})
+
+timeLineAnimation.to(dots,{
+                    scale:1,
+                    repeat:-1,
+                    ease:"smooth",
+                    duration:6,
+                    yoyo:true
+    
+})
+
+}
 
 $(window).scroll(function () {
 
@@ -91,19 +141,8 @@ document.addEventListener("DOMContentLoaded", function () {
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
 
-// Split Screen in About section
-$(document).ready (function() {
-    var controller = new ScrollMagic.Controller();
 
-    new ScrollMagic.Scene({
-        duration: '300%',
-        triggerElement: '.about-title',
-        triggerHook: 0
-    })
-    .setPin('.about-title')
-    // .addIndicators()
-    .addTo(controller);
-});
+
 
 // pagination
 function pagination() {
@@ -144,18 +183,7 @@ $(document).on('click', 'a[href^="#"]', function (e) {
 
 
 //*****  Contact form  *****//
-var formContainer = $('#form-container');
 
-bindFormClick();
-//Opening the form
-function bindFormClick() {
-    $(formContainer).on('click', function (e) {
-        e.preventDefault();
-        // toggleForm();
-        //Ensure container doesn't togleForm when open
-        $(this).off();
-    });
-}
 
 //Closing the form
 $('#form-close, .form-overlay').click(function (e) {
@@ -172,40 +200,8 @@ function toggleForm() {
     $('.form-submitted').removeClass('form-submitted');
 }
 
-//Form validation
-$('form').submit(function () {
-    var form = $(this);
-    form.find('.form-error').removeClass('form-error');
-    var formError = false;
 
-    form.find('.input').each(function () {
-        if ($(this).val() == '') {
-            $(this).addClass('form-error');
-            $(this).select();
-            formError = true;
-            return false;
-        } else if ($(this).hasClass('email') && !isValidEmail($(this).val())) {
-            $(this).addClass('form-error');
-            $(this).select();
-            formError = true;
-            return false;
-        }
-    });
 
-    if (!formError) {
-        $('body').addClass('form-submitted');
-        $('#form-head').addClass('form-submitted');
-        setTimeout(function () {
-            $(form).trigger("reset");
-        }, 1000);
-    }
-    return true;
-});
-
-function isValidEmail(email) {
-    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    return pattern.test(email);
-};
 
 //***  Contact card  ***//
 var btn = document.querySelector('.button');
@@ -228,3 +224,4 @@ btn.addEventListener('click', function () {
     }
 
 });
+
